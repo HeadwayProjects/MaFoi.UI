19fad4c2c11ae0ec7da3e3622932abde210808c0
danielprabhakaran-n <prabukaran99@gmail.com>
2023-04-10 21:08:03 +0530

    Chart label update

--- src/components/pages/Vendor/Dashboard/Chart.js
+++ src/components/pages/Vendor/Dashboard/Chart.js
@@ -8,7 +8,7 @@ const CHART_MAPPING = [
 const defaultConfig = {
     tooltip: {
         trigger: 'item',
-        formatter: "{b} : {c}"
+        formatter: "{b}"
     },
     legend: {
         orient: 'vertical',
@@ -26,8 +26,7 @@ const defaultConfig = {
             height: 250,
             label: {
                 show: true,
-                position: 'inner',
-                formatter: '{c}'
+                position: 'inner'
             },
             emphasis: {
                 itemStyle: {
@@ -50,7 +49,7 @@ function Chart({ data, keys }) {
                 const _chart = CHART_MAPPING.find(x => x.key === key) || {};
                 return {
                     value: data[key],
-                    name: _chart.label,
+                    name: `${data[key]} ${_chart.label}`,
                     itemStyle: {
                         color: _chart.color
                     }
@@ -58,8 +57,14 @@ function Chart({ data, keys }) {
             });
             const _legentData = keys.map(key => {
                 const _chart = CHART_MAPPING.find(x => x.key === key) || {};
-                return _chart.label
+                return `${data[key]} ${_chart.label}`
             });
+            const total = _data.reduce((n, { value }) => n + value, 0);
+            _config.series[0].label.formatter = ({ value }) => {
+                const _valueByTotal = value / total;
+                if (_valueByTotal === NaN) return '0 %';
+                return `${_valueByTotal * 100} %`;
+            };
             _config.series[0].data = _data;
             _config.legend.data = _legentData;
             setConfig(_config);
