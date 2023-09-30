cadb7230bca01476251cd7660b831e043eb5fd0d
Santhosh-Pagidala <31021669+Santhosh-Pagidala@users.noreply.github.com>
2023-09-30 19:06:01 +0530

    [Santhosh] Show hide calendar view

--- src/components/pages/ComplianceOwner/Dashboard/DataGrid.tsx
+++ src/components/pages/ComplianceOwner/Dashboard/DataGrid.tsx
@@ -5,20 +5,25 @@ import styles from "./ComplianceOwnerDashboard.module.css";
 import ComplianceOwnerActivities from "../TaskManagement/ComplianceOwnerActivities";
 import { DashboardView } from "../../../../constants/Compliance.constants";
 import ComplianceDashboardCharts from "./ComplianceDashboardCharts";
+import Icon from "../../../common/Icon";
 
 export default function DataGrid({ filters, view, handleCounts }: any) {
     const [changes, setChanges] = useState<any>(null);
+    const [showCalendar, setShow] = useState(true);
     return (
         <>
             {
                 view === DashboardView.CALENDAR &&
-                <div className={`${styles.dashboardGrid} mx-2`}>
+                <div className={`${styles.dashboardGrid} mx-2 ${showCalendar ? '' : 'hideCalendar'}`}>
                     <div className={styles.dashboardGridCol1}>
                         <Calendar handleChange={setChanges} />
                         <ComplianceOwnerDashboardActivities {...changes} filters={filters} />
                     </div>
                     <div className={styles.dashboardGridCol2}>
                         <ComplianceOwnerActivities filters={filters} handleCounts={handleCounts} />
+                    </div>
+                    <div className={styles.expandCollapseIcon} >
+                        <Icon name={showCalendar ? 'double-left' : 'double-right'} action={() => setShow(!showCalendar)} />
                     </div>
                 </div>
             }
