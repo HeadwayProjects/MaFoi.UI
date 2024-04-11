5c0cf8f8508268877b04717487027a616c9c1fdb
Rajendra Chowdary <rajendrachowdarygutha@gmail.com>
2024-04-11 23:50:59 +0530

    Added Input module list to side nav

--- src/backend/request.ts
+++ src/backend/request.ts
@@ -3,7 +3,7 @@ import { getAuthToken } from "./auth";
 
 function getBaseURL() {
     const { protocol, hostname } = window.location;
-    const api_url = hostname.includes('uat') ? `apiprouat.ezycomp.com`: 'apipro.ezycomp.com';
+    const api_url = hostname.includes('uat') ? `apiprouat.ezycomp.com`: 'apiprouat.ezycomp.com';
     // const api_url = 'apiprouat.ezycomp.com'
     return `${hostname === 'localhost' ? 'https:' : protocol}//${api_url}`;
 }
